b157dc791981c6c5555d803eed58ff4cffafa732
Rich Hildred <rhildred@gmail.com>
2020-12-10 21:19:53 -0500

    add contact form

--- contact.js
+++ contact.js
@@ -2,11 +2,33 @@ const Page = require("./_layout/Default");
 
 module.exports = class extends Page {
     constructor(){
-        super({title:"Contact", sName:"Richard Hildred"});
+        // change localhost to heroku here
+        // and the name to yours
+        super({title:"Contact", sName:"Richard Hildred", sAction:"https://dry-bayou-18746.herokuapp.com/send/"});
     }
     render(sPage) {
         return `
         ${this.requireMarked('_pages/contact.md')}
+        <form action="${this.sAction}" method="POST">
+            <!-- add a div.form-group rather than the <br />-->
+            <div class="form-group">
+                <!-- give inputs the class form-control -->
+                <label>Name: <input name="name" placeholder="name" class="form-control" /></label>
+            </div>
+            <div class="form-group">
+                <label>Email:<input name="email" placeholder="email" type="email" class="form-control" /></label>
+            </div>
+            <div class="form-group">
+                <label>Phone Number:<input name="phone" placeholder="telephone" type="tel"
+                        class="form-control" /></label>
+            </div>
+            <div class="form-group">
+                <label>Message:<textarea name="message" placeholder="type your message here"
+                        class="form-control"></textarea></label>
+            </div>
+            <button type="submit">Send Message</button>
+
+        </form>
         `;
     }
 }
