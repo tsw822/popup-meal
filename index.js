3110fa81455f29c239780b88e3fec6a29c242f40
Rich Hildred <rhildred@gmail.com>
2021-04-01 13:46:40 -0400

    added basic order functionality

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fetch = require('sync-fetch');
 const Page = require("./_layout/Default");
 
 module.exports = class extends Page {
@@ -5,9 +6,24 @@ module.exports = class extends Page {
         super({title:"Home", sName:"Richard Hildred"});
     }
     render(sPage) {
-        return `
-        <img src="${this.resizeImage(`${__dirname}/img/lake.jpg`, 400)}" alt="lake"/>
-        ${this.requireMarked('_pages/index.md')}
-        `;
+        const oJson = fetch("https://ux308winter2021-default-rtdb.firebaseio.com/meals.json").json();
+        console.log(oJson);
+        let sResult = "<h1>Upcoming Popup Meals</h1>";
+        Object.keys(oJson).map((key) => {
+            const oEntity = oJson[key];
+            console.log(oEntity);
+            oEntity.id = key;
+            sResult += `
+            <h2>${oEntity.title}</h2>
+            <p><img src="${oEntity.featured_image}" alt="${oEntity.title}"</p>
+            <p>${oEntity.full_description}</p>
+            <form action="http://localhost:3001/payment" method="post">
+            <input type="hidden" value="${oEntity.title}" />
+            <input type="tel" placeholder="enter your number" />
+            <button type="submit">Order now</button>
+            </form>
+            `;
+        });
+        return sResult;
     }
 }
