0ff549bd35a1a39e9b1064343cf80c8c3bf93d03
Tan Shuwen <tsw822@gmail.com>
2021-04-20 18:47:06 -0400

    fix name and total undifined

--- index.js
+++ index.js
@@ -71,28 +71,26 @@ module.exports = class extends Page {
     </form>
     <form action="http://localhost:3002/payment" method="post">
 
-          <div class="order">
+    <div class="order">
 
-            <div class="card mt-3" style="width: 18rem;">
-              <div class="card-body">
-                <p class="card-title" id="orderResult">Please check your order here.</p>
-                <br>
-                <p class="card-text"><input type="hidden" id="OrderId" value="" /></p>
-                <p class="card-text"><input type="hidden" id="Total" value="" /></p>
-              </div>
-            </div> 
+      <div class="card mt-3" style="width: 18rem;">
+        <div class="card-body">
+          <p class="card-title" id="orderResult">Please check your order here.</p>
+          <br>
+          <p class="card-text"><input type="hidden" id="orderId" name="orderId" value="" /></p>
+          <p class="card-text"><input type="hidden" id="total" name="total" value="" /></p>
+        </div>
+      </div> 
 
-              <div class="input-group mb-3 mt-3">
-                <input class="form-control" type="tel" placeholder="Enter your phone number" />
-                <div class="input-group-append">
-                  <button type="submit" class="btn btn-warning">Pay Now</button>
-                </div>
-              </div>
-
+        <div class="input-group mb-3 mt-3">
+          <input class="form-control" type="tel" name="phone" id="phone" placeholder="Enter your phone number" />
+          <div class="input-group-append">
+            <button type="submit" class="btn btn-warning">Pay Now</button>
           </div>
-        </form> 
+        </div>
 
-
+    </div>
+  </form>
               `;
     return sResult;
   }
